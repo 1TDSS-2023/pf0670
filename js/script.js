a485d919d9fe6a8246c3c67d1f5e8c69605f66b3
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-05-05 20:14:25 -0300

    Concatenação de Arrays com SPREAD(...)

--- js/script.js
+++ js/script.js
@@ -48,40 +48,71 @@
 
 
 //Declaração de um ARRAY e seus métodos!
-let frutas = ["banana","maçã","laranja","morango","uva","amora","cereja","abacaxi"];
-//Imprimindo um array
-console.log(frutas)
-console.log(frutas[7])
-//Verficando o tamanho do array através da propriedade length.
-console.log(`O tamanho do Array é : ${frutas.length}`);
+// let frutas = ["banana","maçã","laranja","morango","uva","amora","cereja","abacaxi"];
+// //Imprimindo um array
+// console.log(frutas)
+// console.log(frutas[7])
+// //Verficando o tamanho do array através da propriedade length.
+// console.log(`O tamanho do Array é : ${frutas.length}`);
 
-//Inserindo um item ao final do array com o método push(nomeitem);
-frutas.push("pêssego");
-console.log(frutas)
-//Inserindo um item no início do array com o método unshift(nomeitem);
-frutas.unshift("kiwi");
-console.log(frutas);
+// //Inserindo um item ao final do array com o método push(nomeitem);
+// frutas.push("pêssego");
+// console.log(frutas)
+// //Inserindo um item no início do array com o método unshift(nomeitem);
+// frutas.unshift("kiwi");
+// console.log(frutas);
 
-//Removendo um item ao final do array com o método pop();
-frutas.pop();
-console.log(frutas);
-//Removendo um item no início do array com o método shift();
-frutas.shift();
-console.log(frutas);
+// //Removendo um item ao final do array com o método pop();
+// frutas.pop();
+// console.log(frutas);
+// //Removendo um item no início do array com o método shift();
+// frutas.shift();
+// console.log(frutas);
 
-//Localizando um item no array com o método indexOf(nomeitem);
-//Obs: O método indexOf(nomeitem) retorna o indice do item.
-// let indice = frutas.indexOf("uva");
-// console.log(`Indice do item buscado ${indice}`);
-// console.log(`Elemento buscado : ${frutas[indice]}`);
+// //Localizando um item no array com o método indexOf(nomeitem);
+// //Obs: O método indexOf(nomeitem) retorna o indice do item.
+// // let indice = frutas.indexOf("uva");
+// // console.log(`Indice do item buscado ${indice}`);
+// // console.log(`Elemento buscado : ${frutas[indice]}`);
 
-//Removendo um item do array com o método splice(índice do item, qtd de vezes que o índice será removido!)
-//Obs: Utilize o método indexOf(nomeItem) para descobrir o índice do item.
-let indice = frutas.indexOf("amora");
-console.log(`Elemento no índice antes da remoção : ${frutas[indice]}`);
-frutas.splice(indice,1);
-console.log(frutas);
-console.log(`Elemento no índice após a remoção : ${frutas[indice]}`);
+// //Removendo um item do array com o método splice(índice do item, qtd de vezes que o índice será removido!)
+// //Obs: Utilize o método indexOf(nomeItem) para descobrir o índice do item.
+// let indice = frutas.indexOf("amora");
+// console.log(`Elemento no índice antes da remoção : ${frutas[indice]}`);
+// frutas.splice(indice,1);
+// console.log(frutas);
+// console.log(`Elemento no índice após a remoção : ${frutas[indice]}`);
 
+// //Impressão do aray em tabela
+// console.table(frutas);
 
+// frutas.forEach( (fruta)=>{
+//     console.log("FRUTA DA VEZ : " + fruta);
+// } );
 
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log("ARRAY 1 : " + nr1);
+// console.log("ARRAY 2 : " + nr2);
+// //Concatenando arrays em um novo array.
+// let nr3 = [nr1,nr2];
+// console.log("NOVO ARRAY : " + nr3);
+// //Imprimindo o novo array com forEach
+// nr3.forEach((nr)=>{
+//     nr.forEach((n)=>{
+//         console.log("ITENS DO NOVO ARRAY : " + n);
+//     });
+// });
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log("ARRAY 1 : " + nr1);
+console.log("ARRAY 2 : " + nr2);
+//Concatenando arrays em um novo array com operador SPREAD( ... );
+let nr3 = [...nr1,...nr2];
+console.log("NOVO ARRAY : " + nr3);
+//Imprimindo o novo array com forEach
+nr3.forEach((nr)=>{
+    // nr.forEach((n)=>{
+        console.log("ITENS DO NOVO ARRAY : " + nr);
+    // });
+});
